42d831786e18940e59d1ef63211b274bb69e0907
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-16 19:00:18 +0200

    fix: fix input errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 meaning success
-* Description: Program returns data types
-* found on the host computer
-*/
-int main(void)
-{
-printf("The size of a char: %ld byte(s) \n", sizeof(char));
-printf("The size of an int: %ld byte(s) \n", sizeof(int));
-printf("The size of a long int: %ld byte(s) \n", sizeof(int));
-printf("The size of a long long int: %ld byte(s) \n", sizeof(int) + sizeof(int));
-printf("The size of a float: %ld byte(s) \n", sizeof(float));
-return (0);
+ * main - Entry point
+ *
+ * Return: Always 0 meaning success
+ * Description: Program returns data types
+ * found on the host computer
+ */
+int main(void) {
+  printf("Size of a char: %ld byte(s)\n", sizeof(char));
+  printf("Size of an int: %ld byte(s)\n", sizeof(int));
+  printf("Size of a long int: %ld byte(s)\n", sizeof(int));
+  printf("Size of a long long int: %ld byte(s)\n", sizeof(int) + sizeof(int));
+  printf("Size of a float: %ld byte(s)\n", sizeof(float));
+  return (0);
 }
