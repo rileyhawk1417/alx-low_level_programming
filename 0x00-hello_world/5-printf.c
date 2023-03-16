3c03623e934683e997032d14f3f8704afe6d2488
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-16 19:48:17 +0200

    fix: fix betty linting & add pipes on task #1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,8 @@
  * Description: Program simply returns a string
  * using the printf function
  */
-int main(void) {
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
