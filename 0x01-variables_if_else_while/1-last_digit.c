77a29683cf1f8f9b8d5d0bbadfd4f081fb8d4e33
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-19 16:36:00 +0200

    fix: replace variables with numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ if (n % 10 > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, n);
 
 if (n % 10 == 0)
-printf("Last digit of %d is %d and is %d\n", n, n, n);
+printf("Last digit of %d is %d and is %d\n", n, 0, 0);
 
 if (n % 10 < 6 && n % 10 != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n);
