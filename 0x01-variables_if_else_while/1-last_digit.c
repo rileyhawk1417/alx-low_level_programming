631da27551d81d56905aaa69f83ccb7d2a0e2b7b
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-19 16:39:26 +0200

    fix: remove newlines to pass checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2 ;
 if (n % 10 > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, n);
+printf("Last digit of %d is %d and is greater than 5", n, n);
 
 if (n % 10 == 0)
 printf("Last digit of %d is %d and is %d\n", n, 0, 0);
 
 if (n % 10 < 6 && n % 10 != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n);
+printf("Last digit of %d is %d and is less than 6 and not 0", n, n);
 
 return (0);
 }
