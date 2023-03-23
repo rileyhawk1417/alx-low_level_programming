4e1e048ca47f0925b754319bfd612704e34ab5cd
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-23 16:44:41 +0200

    fix: fix code for 2 clases

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ for (numA = 0; numA < 10; numA++)
 {
 for (numB = 0; numB <= 14; numB++)
 {
-if (numB < 10)
+if (numB > 9)
 {
-_putchar((numB) + '0');
+_putchar((numB / 10) + '0');
 }
-else if (numB > 9)
-{
-_putchar((1) + '0');
 _putchar((numB % 10) + '0');
-}
 }
 _putchar('\n');
 }
