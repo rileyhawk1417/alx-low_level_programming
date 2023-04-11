4021456d39bc52494fc33f8c7aed242252c00499
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-04-11 10:23:35 +0200

    fix: correct betty linting

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,11 +8,13 @@
  * Description: Print the arguments per line
  * Return: Always 0.
  */
-int main(int argc, char *argv[]) {
-  int i = 0;
+int main(int argc, char *argv[])
+{
+int i = 0;
 
-  for (i = 0; i < argc; i++) {
-    printf("%s\n", argv[i]);
-  }
-  return (0);
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
