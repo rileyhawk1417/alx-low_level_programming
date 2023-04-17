efd1d790f8057b62b4937118b1f86800b57da15d
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-04-17 15:15:49 +0200

    fix: correct with betty linting

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-  printf(__FILE__ "\n");
-  return (0);
+/**
+ * main - Entry point
+ * Return: 0
+ * Description: Simply prints the current file name
+ */
+int main(void)
+{
+printf(__FILE__ "\n");
+return (0);
 }
