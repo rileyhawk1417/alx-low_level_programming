f17a838d9b094f33c4e3e26d0f9e6d49baba49e1
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-04-18 15:10:27 +0200

    chore: copy text to new array & add null cases

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
-#include <string.h>
 #include <stdlib.h>
-
 /**
  * new_dog - Entry point
  * @name: char value
@@ -9,36 +7,46 @@
  * @owner: char value
  * Return: returns nothing
  * Description: The function prints the struct of dog
+ * Sorry for squashed code needs to pass betty linting
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int len1 = 0, len2 = 0;
-  dog_t *dogs;
-
+int len1 = 0, len2 = 0, i = 0;
+dog_t *dogs;
+/* NOTE: strlen usually leaves out null character using while is better */
 while (len1[name] != '\0')
+{
 len1++;
-
+}
 while (len2[owner] != '\0')
+{
 len2++;
-
+}
 dogs = malloc(sizeof(dog_t));
-
 if (dogs == NULL)
 {
 free(dogs);
 return (NULL);
 }
-
+/* NOTE: strcpy didnt work so use malloc to assign then for loop to copy */
 dogs->name = malloc(sizeof(dogs->name) * len1);
 if (dogs->name == NULL)
 {
-free(dogs->name);
 free(dogs);
-return (NULL);
 }
-dogs->name =  name;
-dogs->owner =  owner;
+for (i = 0; i <= len1; i++)
+{
+dogs->name[i] = name[i];
+}
+dogs->owner = malloc(sizeof(dogs->owner) * len2);
+if (dogs->owner == NULL)
+{
+free(dogs);
+}
+for (i = 0; i <= len2; i++)
+{
+dogs->owner[i] = owner[i];
+}
 dogs->age = age;
-
 return (dogs);
 }
