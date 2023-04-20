b1af9abea9731247c62c52f5f3863ae57b9d8488
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-04-20 11:35:10 +0200

    chore: add null checks & switch to variables

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,12 +11,35 @@
  */
 int main(int argc, char *argv[])
 {
-int value = 0;
-/* TODO: Add null test cases when checker fails */
-if (argc > 1)
+int value = 0, a = 0, b = 0;
+char *operator;
+
+/* Just have to use argc somewhere */
+if (argc != 4)
 {
-value = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+printf("Error\n");
+exit(98);
 }
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+operator = argv[2];
+
+value = get_op_func(operator)(a, b);
+/* Test case for get_op_func & first input */
+if (get_op_func(operator) == NULL || operator[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+
+/* Test case for operator & last input number need to use pointers */
+if ((*operator == '/' && b == '\0') || (*operator == '%' && b == 0))
+{
+printf("Error\n");
+exit(100);
+}
+
 
 printf("%d\n", value);
 return (0);
