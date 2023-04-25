08f1e401d844cfe758a9cb459bd61c27fc11d00c
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-04-25 11:42:56 +0200

    feat: complete task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_all - Entry point
+* @format: format modifier char value
+* Return: nothing
+* Description: Similar to printf function '%' is not used
+*/
+/*NOTE:Since there are no '%' for modifiers we have to manually*/
+/*set the state to 1 when printing. To build printf use conditions for state*/
+void print_all(const char *const format, ...)
+{
+int i = 0, x, state = 0;
+char *s;
+const char mods[] = "cifs";
+va_list args;
+va_start(args, format);
+
+while (format && format[i])
+{
+x = 0;
+while (mods[x])
+{
+	if (format[i] == mods[x] && state)
+	{
+		printf(", ");
+		break;
+	} x++;
+}
+switch (format[i])
+{
+case 'c':
+	putchar((char)va_arg(args, int)), state = 1;
+	break;
+case 's':
+	s = va_arg(args, char *);
+	while (!s)
+	{
+		printf("(nil)");/* yes am lazy to use putchar on this one */
+		break;
+	}
+	printf("%s", s), state = 1;
+	break;
+case 'i':
+	putchar(va_arg(args, int) + '0'), state = 1;
+	break;
+case 'f':
+	putchar(va_arg(args, double) + '0'), state = 1;
+	break;
+} i++;
+}
+printf("\n"), va_end(args);
+}
