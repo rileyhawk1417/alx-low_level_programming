8bc64db3ef013f283a2a42cb26888c0caf77dfd7
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-04-28 09:43:44 +0200

    fix: move list counter to main loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,17 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t x = h->len;
+size_t x = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
 	printf("[0] (nil)\n");
-else
-	printf("[%u] %s\n", h->len, h->str);
+else 
+{
+printf("[%u] %s\n", h->len, h->str);
+}
 
+x++;
 h = h->next;
 }
 return (x);
