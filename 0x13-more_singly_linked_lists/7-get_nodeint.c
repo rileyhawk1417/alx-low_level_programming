b1290bd6c342a527a1c21393618eb28aa358c620
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-05-03 12:08:23 +0200

    chore: fix betty linting

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,15 +9,17 @@
  * then returns the single node as a list.
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index) {
-  unsigned int count = 0;
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int count = 0;
 
-  while (count < index) {
-    if (head == NULL) {
-      return (NULL);
-    }
-    head = head->next;
-    count++;
-  }
-  return (head);
+while (count < index)
+{
+if (head == NULL)
+	return (NULL);
+
+head = head->next;
+count++;
+}
+return (head);
 }
