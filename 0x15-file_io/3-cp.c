10e36455963c1fc9077e17bad0c8061437fd3643
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-05-17 15:16:32 +0200

    refactor: include missing functions & conditions
    Add conditions needed by the test case

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,21 +12,42 @@
  */
 int main(int argc, char *argv[])
 {
-int src, dest, read_file;
-char buffer[1024];
+int src, dest, read_file, closeSrcSignal, closeDestSignal, writeDest;
+char buffer[BUFSIZ];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
 }
-
 src = open(argv[1], O_RDONLY);
-
+if (src < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
 dest = open(argv[2], O_CREAT | O_WRONLY, 0664);
-
-read_file = read(src, buffer, 1024);
-write(dest, buffer, read_file);
-
+read_file = read(src, buffer, BUFSIZ);
+if (read_file < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from  file %s\n", argv[1]);
+exit(98);
+}
+writeDest = write(dest, buffer, read_file);
+if (dest < 0 || writeDest != read_file)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to  file %s\n", argv[2]);
 close(src);
-close(dest);
+exit(99);
+}
+closeSrcSignal = close(src);
+closeDestSignal = close(dest);
+if (closeSrcSignal < 0 || closeDestSignal < 0)
+{
+if (closeSrcSignal < 0)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
+if (closeDestSignal < 0)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
+exit(100);
+}
 return (0);
 }
