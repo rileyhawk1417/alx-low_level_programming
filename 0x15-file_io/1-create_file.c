2f3f9881b536841c47284746920181b7bf3d2466
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-05-17 11:53:23 +0200

    fix: solve failing checks in task #1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,28 +12,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int result, fd;
+int result = 1, fd, stringLen = 0, output_length;
 
 if (filename == NULL)
 {
 result = -1;
 }
 
-fd = open(filename, O_RDWR | O_CREAT);
-
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (fd == -1)
 {
 result = -1;
 }
-
 if (text_content != NULL)
 {
-write(fd, text_content, (strlen(text_content) + 1));
+stringLen = strlen(text_content);
+output_length = write(fd, text_content, stringLen);
+if (output_length != stringLen)
+	result = -1;
 }
 
 close(fd);
-
-result = 1;
 
 return (result);
 }
