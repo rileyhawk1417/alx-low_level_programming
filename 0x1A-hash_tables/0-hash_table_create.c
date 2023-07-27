401a015e2ff010c100698eae414e5af72e1c8a6f
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-07-27 15:42:45 +0200

    fix: add null check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
+	return (NULL);
 
 ht->size = size;
 ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
