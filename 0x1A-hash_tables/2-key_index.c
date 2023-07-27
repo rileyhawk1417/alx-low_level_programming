a009351f2c30e96f4646e9f7f33f5ab5a3f1fe6e
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-07-27 16:05:32 +0200

    docs: add function docs

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - Fetch the key
+ *
+ * @key: The key to identify
+ * @size: the size of the array
+ * Description: Fetches the index using the key
+ * Return: hash table key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 return (hash_djb2(key) % size);
