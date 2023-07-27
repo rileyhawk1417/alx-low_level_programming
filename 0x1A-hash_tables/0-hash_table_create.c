9aa69c0062c9f554e8fe312759018c806da88dc0
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-07-27 15:34:46 +0200

    feat: complete task 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of items to store
+ * Description: Simply creates a hash table
+ * Return: New HashTable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+ht->size = size;
+ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+return (ht);
+}
